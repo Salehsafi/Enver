9906546e6aff85a17d3da7864fd6427d8d9839f9
Salehsafi <107122794+Salehsafi@users.noreply.github.com>
2023-09-26 08:58:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+   
+    $(window).on('load', function(){
+        $('.loader').fadeOut(1000);
+    })
+
+
+    $('.nav-link').click(function(){
+        $('.nav-item').find('.nav-link').removeClass('active')
+        $(this).addClass('active')
+    })
+   
+    $(".content .owl-carousel").owlCarousel({
+        loop:true,
+        margin:60,
+        dots:true,
+        autoplay:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:3
+            }
+        }});
+
+    $(".owl-carousel").owlCarousel();
+});
